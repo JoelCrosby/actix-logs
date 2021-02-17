547f48c36c9f02d4a6cf180095391b14197de1af
Joel Crosby <joelcrosby@live.co.uk>
2021-02-17 01:59:46 +0000

    add migrations

--- migrations/2021-02-15-211647_create_logs/up.sql
+++ migrations/2021-02-15-211647_create_logs/up.sql
@@ -2,5 +2,12 @@ CREATE TABLE logs (
   id SERIAL PRIMARY KEY,
   title VARCHAR(256) NOT NULL,
   serialised json,
-  created_at timestamp NOT NULL DEFAULT Now()
+  created_at TIMESTAMP NOT NULL DEFAULT Now()
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(256) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT Now()
 );
